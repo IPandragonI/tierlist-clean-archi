47ffb6e7f27f2e4c701d6b2885ea7e6b80ebef81
IPandragonI <mathysf84pro@gmail.com>
2026-01-06 12:11:27 +0100

    Fix

--- backend/src/main/java/fr/esgi/tierlist/application/form/TierListForm.java
+++ backend/src/main/java/fr/esgi/tierlist/application/form/TierListForm.java
@@ -10,7 +10,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TierListForm {
-    private Long id;
 
     @NotBlank(message = "Name is required")
     private String name;
